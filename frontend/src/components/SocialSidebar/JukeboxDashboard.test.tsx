a0171aa5a5a004e7b92320ad3284f2769be86838
mscMstr <delgadocarla2021@gmail.com>
2023-11-02 01:18:48 -0400

    wrote a preliminary test plan for JukeboxDashboard.tsx

---
+++ frontend/src/components/SocialSidebar/JukeboxDashboard.test.tsx
@@ -0,0 +1,37 @@
+describe('Jukebox sidebar dashboard', () => {
+  describe('Test SongDisplay', () => {
+    test('song name and artist correctly rendered', () => {
+      // write test here
+    });
+  });
+  describe('Test SongQueueItemDisplay', () => {
+    test('all queue items correctly rendered', () => {
+      // write test here
+    });
+    test('upvote button click updates upvote value', () => {
+      // write test here
+    });
+    test('downvote button click updates downvote value', () => {
+      // write test here
+    });
+  });
+  describe('Test JukeboxDashboardView', () => {
+    it('correctly render current song', () => {
+      // write test here
+    });
+    it('correctly render queue', () => {
+      // write test here
+    });
+    it('correctly handles vote for songs', () => {
+      // write test here
+    });
+  });
+  describe('Test JukeboxDashboard', () => {
+    it('renders jukebox dashboard if player is in the jukebox area', () => {
+      // write test here
+    });
+    it('does not render jukebox dashboard if there are no players in the area', () => {
+      // write test here
+    });
+  });
+});
