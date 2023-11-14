c35c9975172cb33708d9769f71d26fc93dd44f4c
Yuchen-Zhang-Karen <karenzyc@gmail.com>
2023-11-14 16:00:29 -0500

    Revert "integrated youtube search api call with the form search button and display"
    This reverts commit 70013c05b6841bd0027e84b0953bf6899a9392a9.
    
    Revert changes to suggestion form

--- frontend/src/components/SocialSidebar/SuggestionForm.tsx
+++ frontend/src/components/SocialSidebar/SuggestionForm.tsx
@@ -3,53 +3,23 @@ import {
   Container,
   Flex,
   Input,
-  ListItem,
   Modal,
   ModalCloseButton,
   ModalContent,
   ModalHeader,
   ModalOverlay,
-  OrderedList,
 } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import { Song } from '../../../../shared/types/CoveyTownSocket';
+import React from 'react';
 
 type SuggestionFormProps = {
   showForm: boolean;
   handleClose: () => void;
 };
 
-function SuggestionForm({handleSearch}:{handleSearch: (searchResults: Song[]) => void}): JSX.Element {
+function SuggestionForm(): JSX.Element {
   /* - song and artist input will use form input component
   - results will be a scrollable flex container. chakra has a flex container.
   - Search and addtoqueue should be wrapped around containers so we can justify it to the right */
-
-  // return <Button onClick={() => searchSong("Style", "Taylor Swift")}>search</Button>;
-
-  function searchSong({songName, artistName, handleSearch} : {songName: string, artistName: string, handleSearch: (searchResults: Song[]) => void}){
-    console.log("HERE");
-    const YOUTUBE_API_KEY = `AIzaSyAwJ9S1H92Mx9QqbEMBrDew49NxfMcar6w`;
-    console.log(YOUTUBE_API_KEY);
-    const searchTerm = `${songName}+${artistName}`;
-    const numResults = 10;
-    let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${numResults}&q=${searchTerm}&key=${YOUTUBE_API_KEY}`;
-    url = encodeURI(url);
-    console.log(url);
-
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.items);
-        console.log(data.items[0].id.videoId);
-        // console.log(data.items[1].id.videoId);
-        let searchResults:Song[] = [];
-        data.items.map(result => {
-          searchResults.push({songName: result.snippet.title, artistName: result.snippet.channelTitle, videoId: result.id.videoId});
-        })
-        handleSearch(searchResults);
-    });
-}
-
   return (
     <Container>
       <Input placeholder='Song name' />
@@ -60,44 +30,24 @@ function SuggestionForm({handleSearch}:{handleSearch: (searchResults: Song[]) =>
         <Container></Container>
       </Flex>
       <Flex>
-        <Button onClick={() => searchSong({songName: "Style", artistName: "Taylor Swift", handleSearch: handleSearch})}>search</Button>
+        <Button>search</Button>
         <Button>add</Button>
       </Flex>
     </Container>
   );
 }
 
-function SearchResultDisplay({searchResult}:{searchResult: Song[]}): JSX.Element {
-  if (searchResult.length == 0) {
-    return <></>;
-  }
-  return (
-    <OrderedList>
-        {searchResult.map(searchResultItem => {
-          return (
-            <ListItem key={searchResultItem.videoId}>
-              {searchResultItem.songName}
-              {searchResultItem.artistName}
-            </ListItem>
-          );
-        })}
-      </OrderedList>
-  )
-}
-
 export default function SuggestionFormWrapper({
   showForm,
   handleClose,
 }: SuggestionFormProps): JSX.Element {
-  const [searchResult, setSearchResult] = useState<Song[]>([]);
   return (
     <Modal isOpen={showForm} onClose={handleClose} closeOnOverlayClick={false}>
       <ModalOverlay />
       <ModalContent minH='80vh'>
         <ModalHeader>{'Song Suggest'}</ModalHeader>
         <ModalCloseButton />
-        <SuggestionForm handleSearch={setSearchResult}/>
-        <SearchResultDisplay searchResult={searchResult}/>
+        <SuggestionForm />
       </ModalContent>
     </Modal>
   );
